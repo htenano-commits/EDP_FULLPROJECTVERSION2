e3195e25a6822ca6b017885fb7bb19bbea4cf105
htenano-commits <htenano@pcu.edu.ph>
2026-03-16 15:32:13 +0800

    first commit

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HerkLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (tbUsername.Text == "")
+            {
+                MessageBox.Show("Please enter username.", "Username required");
+                tbUsername.Focus();
+            }
+            else if (tbPassword.Text == "")
+            {
+                MessageBox.Show("Please enter a password", "Password required");
+                tbPassword.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Welcome " + tbUsername.Text);
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
