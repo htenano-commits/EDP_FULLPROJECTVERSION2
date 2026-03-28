da0bd5f128a891ddb8e90d00b972bc02ac78ab2d
htenano <htenano@pcu.edu.ph>
2026-03-27 20:47:32 -1000

    Add files via upload

---
+++ thestore.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace StoreSystemApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlRegister = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlStore = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pnlPrice = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pnlVendors = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pnlUsers = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pnlTime = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlRegister.SuspendLayout();
+            this.pnlStore.SuspendLayout();
+            this.pnlPrice.SuspendLayout();
+            this.pnlVendors.SuspendLayout();
+            this.pnlUsers.SuspendLayout();
+            this.pnlTime.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlRegister
+            // 
+            this.pnlRegister.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnlRegister.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlRegister.BackgroundImage")));
+            this.pnlRegister.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlRegister.Controls.Add(this.label2);
+            this.pnlRegister.Location = new System.Drawing.Point(147, 179);
+            this.pnlRegister.Name = "pnlRegister";
+            this.pnlRegister.Size = new System.Drawing.Size(126, 100);
+            this.pnlRegister.TabIndex = 0;
+            this.pnlRegister.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlRegister_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(-3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Register";
+            // 
+            // pnlStore
+            // 
+            this.pnlStore.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnlStore.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlStore.BackgroundImage")));
+            this.pnlStore.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlStore.Controls.Add(this.label3);
+            this.pnlStore.Location = new System.Drawing.Point(304, 179);
+            this.pnlStore.Name = "pnlStore";
+            this.pnlStore.Size = new System.Drawing.Size(126, 100);
+            this.pnlStore.TabIndex = 1;
+            this.pnlStore.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlStore_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(-3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Store Status";
+            // 
+            // pnlPrice
+            // 
+            this.pnlPrice.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnlPrice.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlPrice.BackgroundImage")));
+            this.pnlPrice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlPrice.Controls.Add(this.label4);
+            this.pnlPrice.Location = new System.Drawing.Point(465, 179);
+            this.pnlPrice.Name = "pnlPrice";
+            this.pnlPrice.Size = new System.Drawing.Size(126, 100);
+            this.pnlPrice.TabIndex = 1;
+            this.pnlPrice.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlPrice_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(-3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Pricebook";
+            // 
+            // pnlVendors
+            // 
+            this.pnlVendors.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnlVendors.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlVendors.BackgroundImage")));
+            this.pnlVendors.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlVendors.Controls.Add(this.label5);
+            this.pnlVendors.Location = new System.Drawing.Point(147, 304);
+            this.pnlVendors.Name = "pnlVendors";
+            this.pnlVendors.Size = new System.Drawing.Size(126, 100);
+            this.pnlVendors.TabIndex = 1;
+            this.pnlVendors.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlVendors_Paint);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(-3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Vendors";
+            // 
+            // pnlUsers
+            // 
+            this.pnlUsers.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnlUsers.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlUsers.BackgroundImage")));
+            this.pnlUsers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlUsers.Controls.Add(this.label6);
+            this.pnlUsers.Location = new System.Drawing.Point(304, 304);
+            this.pnlUsers.Name = "pnlUsers";
+            this.pnlUsers.Size = new System.Drawing.Size(126, 100);
+            this.pnlUsers.TabIndex = 1;
+            this.pnlUsers.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlUsers_Paint);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(-3, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Users";
+            // 
+            // pnlTime
+            // 
+            this.pnlTime.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnlTime.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlTime.BackgroundImage")));
+            this.pnlTime.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlTime.Controls.Add(this.label7);
+            this.pnlTime.Location = new System.Drawing.Point(465, 304);
+            this.pnlTime.Name = "pnlTime";
+            this.pnlTime.Size = new System.Drawing.Size(126, 100);
+            this.pnlTime.TabIndex = 1;
+            this.pnlTime.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlTime_Paint);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(-3, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Time Clock";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(315, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(744, 587);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlTime);
+            this.Controls.Add(this.pnlUsers);
+            this.Controls.Add(this.pnlVendors);
+            this.Controls.Add(this.pnlPrice);
+            this.Controls.Add(this.pnlStore);
+            this.Controls.Add(this.pnlRegister);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlRegister.ResumeLayout(false);
+            this.pnlRegister.PerformLayout();
+            this.pnlStore.ResumeLayout(false);
+            this.pnlStore.PerformLayout();
+            this.pnlPrice.ResumeLayout(false);
+            this.pnlPrice.PerformLayout();
+            this.pnlVendors.ResumeLayout(false);
+            this.pnlVendors.PerformLayout();
+            this.pnlUsers.ResumeLayout(false);
+            this.pnlUsers.PerformLayout();
+            this.pnlTime.ResumeLayout(false);
+            this.pnlTime.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlRegister;
+        private System.Windows.Forms.Panel pnlStore;
+        private System.Windows.Forms.Panel pnlPrice;
+        private System.Windows.Forms.Panel pnlVendors;
+        private System.Windows.Forms.Panel pnlUsers;
+        private System.Windows.Forms.Panel pnlTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ thestore.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StoreSystemApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Panel[] panels = { pnlRegister, pnlStore, pnlPrice, pnlVendors, pnlUsers, pnlTime };
+
+            foreach (Panel pnl in panels)
+            {
+                pnl.Cursor = Cursors.Hand;
+                pnl.Click += Panel_Click;
+                pnl.MouseEnter += Panel_MouseEnter;
+                pnl.MouseLeave += Panel_MouseLeave;
+            }
+        }
+
+        // reuse 
+        private void OpenForm(Form f)
+        {
+            f.WindowState = FormWindowState.Maximized;
+            f.StartPosition = FormStartPosition.CenterScreen;
+            f.Show();
+        }
+
+        // ================= CLICK =================
+        private void Panel_Click(object sender, EventArgs e)
+        {
+            if (sender is Panel pnl)
+            {
+                switch (pnl.Name)
+                {
+                    case "pnlRegister":
+                        // options 
+                        //open frm rg
+
+                        MessageBox.Show("Register clicked");
+                        break;
+
+                    case "pnlStore":
+                        // OpenForm new frmStatusStore
+                        MessageBox.Show("Store Status clicked");
+                        break;
+
+                    case "pnlPrice":
+                        // OpenForm new frmPricebook
+                        MessageBox.Show("Pricebook clicked");
+                        break;
+
+                    case "pnlVendors":
+                        // OpenForm new frmVendors
+                        MessageBox.Show("Vendors clicked");
+                        break;
+
+                    case "pnlUsers":
+                        // OpenForm new frmUsers
+                        MessageBox.Show("Users clicked");
+                        break;
+
+                    case "pnlTime":
+                        // OpenForm new frmTimeClock
+                        MessageBox.Show("Time Clock clicked");
+                        break;
+                }
+            }
+        }
+
+        // ================= HOVER =================
+        private void Panel_MouseEnter(object sender, EventArgs e)
+        {
+            if (sender is Panel pnl)
+                pnl.BackColor = Color.LightGray;
+        }
+
+        private void Panel_MouseLeave(object sender, EventArgs e)
+        {
+            if (sender is Panel pnl)
+                pnl.BackColor = Color.White;
+        }
+
+        // ================= DESIGN EVENTS =================
+        private void pnlRegister_Paint(object sender, PaintEventArgs e) { }
+        private void pnlStore_Paint(object sender, PaintEventArgs e) { }
+        private void pnlPrice_Paint(object sender, PaintEventArgs e) { }
+        private void pnlVendors_Paint(object sender, PaintEventArgs e) { }
+        private void pnlUsers_Paint(object sender, PaintEventArgs e) { }
+        private void pnlTime_Paint(object sender, PaintEventArgs e) { }
+    }
+}
